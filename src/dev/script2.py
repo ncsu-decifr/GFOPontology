19b9cfa849c91a049842ce61a0d683731eef341a
jim <jbwhite2@ncsu.edu>
2023-05-23 21:01:29 -0400

    libraries

--- src/dev/script2.py
+++ src/dev/script2.py
@@ -5,21 +5,27 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-file = "https://external.gnps2.org/gnpslibrary"
 
-x = requests.get(file)
-print(x)
+def main():
+    file = "https://external.gnps2.org/gnpslibrary"
+
+    r = requests.get(file)
+    html_text = r.text
+
+    print(html_text)
+
+    soup = BeautifulSoup(html_text)
+
+    for tag in soup.find_all('script'):
+        print("##############")
+        print(tag.text[:100])
+        print(tag.text[-100:])
+
+    content = soup.find_all('script')[4].text
+    with open("libraries.js", "w") as fp:
+        fp.write(content)
 
 
-
-# soup = BeautifulSoup(original_html_text)
-
-# for tag in soup.find_all('script'):
-#     print("##############")
-#     print(tag.text[:100])
-#     print(tag.text[-100:])
-
-
-
-
+if __name__ == '__main__':
+    main()
 
